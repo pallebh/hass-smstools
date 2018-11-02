602303acd7a1357b73213b87d8e80bab2cb86dd4
palle <pallebh@gmail.com>
2018-11-02 20:24:32 +0100

    fix

--- smstools/run.sh
+++ smstools/run.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 set -e
-chmod o+w /dev/stdout
+echo "echo test"
+#chmod o+w /dev/stdout
 
 CONFIG_PATH=/data/options.json
 SMSDCONF_PATH=/etc/smsd.conf
 
-echo "" > $SMSDCONF_PATH
-INIT=$(jq --raw-output ".options.smstools.init" $CONFIG_PATH)
-if [ -z $INIT ] ;
-	then echo '#init = ""' >> $SMSDCONF_PATH ;
-	else echo "init = " >> $SMSDCONF_PATH ;
-fi
+#echo "" > $SMSDCONF_PATH
+#INIT=$(jq --raw-output ".options.smstools.init" $CONFIG_PATH)
+#if [ -z $INIT ] ;
+#	then echo '#init = ""' >> $SMSDCONF_PATH ;
+#	else echo "init = " >> $SMSDCONF_PATH ;
+#fi
 
-PIN=$(jq --raw-output ".options.smstools.pin" $CONFIG_PATH)
-if [ -z $PIN ] ;
-	then echo '#pin = ""' >> $SMSDCONF_PATH ;
-	else echo "pin = " $PIN >> $SMSDCONF_PATH ;
-fi
+#PIN=$(jq --raw-output ".options.smstools.pin" $CONFIG_PATH)
+#if [ -z $PIN ] ;
+#	then echo '#pin = ""' >> $SMSDCONF_PATH ;
+#	else echo "pin = " $PIN >> $SMSDCONF_PATH ;
+#fi
 	
-DEVICE=$(jq --raw-output ".options.smstools.device" $CONFIG_PATH)
-echo "device = " $DEVICE >> $SMSDCONF_PATH
+#DEVICE=$(jq --raw-output ".options.smstools.device" $CONFIG_PATH)
+#echo "device = " $DEVICE >> $SMSDCONF_PATH
 
-BAUDRATE=$(jq --raw-output ".options.smstools.baudrate" $CONFIG_PATH)
-echo "baudrate = " $BAUDRATE >> $SMSDCONF_PATH
+#BAUDRATE=$(jq --raw-output ".options.smstools.baudrate" $CONFIG_PATH)
+#echo "baudrate = " $BAUDRATE >> $SMSDCONF_PATH
 
-cat $SMSDCONF_PATH 
-smsd -t &
-tail -f /var/log/smsd.log
+#cat $SMSDCONF_PATH 
+#smsd -t &
+#tail -f /var/log/smsd.log
